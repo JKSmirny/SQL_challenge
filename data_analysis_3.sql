7f9bf223a6ccf908a9996ff91ee9a6391546a613
JKSmirny <95670413+JKSmirny@users.noreply.github.com>
2022-02-21 01:27:54 -0500

    Delete data_analysis_3.sql

--- data_analysis_3.sql
+++
@@ -1,9 +0,0 @@
---3. List the manager of each department with the following information:
---department number, department name, the manager's employee number, last name, first name.
-
-select departments.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name
-from departments
-join dept_manager
-on departments.dept_no = dept_manager.dept_no
-join employees
-on dept_manager.emp_no = employees.emp_no;
