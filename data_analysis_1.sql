fd9cf034a544a24c72786664fb62758261caf2af
JKSmirny <95670413+JKSmirny@users.noreply.github.com>
2022-02-21 01:27:41 -0500

    Delete data_analysis_1.sql

--- data_analysis_1.sql
+++
@@ -1,7 +0,0 @@
---List the following details of each employee: 
---employee number, last name, first name, sex, and salary.
-
-select last_name, first_name, sex, salary
-from employees
-inner join salaries
-on employees.emp_no = salaries.emp_no;
