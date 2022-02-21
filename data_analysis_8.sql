e852036e0488371cf4376e244c3062f07f30fe2d
JKSmirny <95670413+JKSmirny@users.noreply.github.com>
2022-02-21 01:28:23 -0500

    Delete data_analysis_8.sql

--- data_analysis_8.sql
+++
@@ -1,6 +0,0 @@
---8. In descending order, list the frequency count of employee 
---last names,i.e., how many employees share each last name.
-select last_name, COUNT(last_name)as frequency
-from employees
-group by last_name
-order by COUNT(last_name) desc;
